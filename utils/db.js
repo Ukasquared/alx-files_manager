570d0cde18c18611d5b9f50c7a0063755697bf00
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-10-03 05:13:55 +0100

    db

---
+++ utils/db.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+
+class DBClient {
+
+    constructor() {
+        this.localhost = process.env.DB_HOST ? process.env.DB_HOST : 'localhost'
+        this.port = process.env.DB_PORT ? process.env.DB_PORT : '27017'
+        this.database = process.env.DB_DATABASE ? process.env.DB_DATABASE : 'files_manager'
+        this.dbUrl = `mongodb://${this.localhost}:${this.port}`;
+ 
+        this.client =  new MongoClient(this.dbUrl, { useUnifiedTopology: true});
+        this.client.connect();
+    }
+
+    isAlive() {
+       return this.client.isConnected();
+    }
+
+    async nbUsers() {
+        return this.client.db(this.database).collection('users').countDocuments();
+
+    }
+
+    async nbFiles() {
+        return this.client.db(this.database).collection('files').countDocuments();
+    
+    }
+}
+
+const dbClient = new DBClient()
+
+export default dbClient;
